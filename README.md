c3a09be50aab128face861963e8b425e751db011
Nadav Tasger <tashernadav@gmail.com>
2019-12-16 20:05:55 +0200

    Progress

---
+++ README.md
@@ -0,0 +1,2 @@
+invite:
+https://discordapp.com/oauth2/authorize?client_id=656177472525828117&scope=bot
--- app/app.js
+++ app/app.js
@@ -1,14 +1,160 @@
+// Requires
 const FileSystem = require('fs');
 const Discord = require('discord.js');
+
+// Discord client initialization
 const Client = new Discord.Client();
 
+// Webappify constants
+const WEBAPPIFY_API = "webappify";
+const WEBAPPIFY_URL = "https://webappify.com/";
+const WEBAPPIFY_ENDPOINT = "scripts/backend/webappify/webappify.php";
+
+let templates = [];
+
+/**
+ * Handle bot startup
+ */
 Client.on('ready', () => {
-    console.log("Connected as " + Client.user.tag)
+    api(WEBAPPIFY_URL + WEBAPPIFY_ENDPOINT, WEBAPPIFY_API, "list", {}, (success, result) => {
+        if (success)
+            templates = result;
+        else
+            console.error(result);
+    });
 });
 
-FileSystem.readFile('./BotSecretToken', function read(error, token) {
-    if (error) {
-        throw error;
+/**
+ * Handle an incoming message
+ */
+Client.on('message', (receivedMessage) => {
+    if (receivedMessage.author !== Client.user) {
+        if (templates.length > 0) {
+            // Loop through templates
+            let template = null;
+            for (let t = 0; t < templates.length && template === null; t++) {
+                // Filter template name
+                let filtered = templates[t].toLowerCase().replace("template", "");
+                // Check whether message matches the regex
+                if (("/^" + filtered + "\(.+\)$/").test(receivedMessage.content)) {
+                    template = templates[t];
+                }
+            }
+            if (template === null){
+                receivedMessage.channel.send("No such template / Wrong syntax");
+            } else {
+                receivedMessage.channel.send("Ok, creating app with template "+template);
+            }
+        }
+        if (receivedMessage.content.includes(Client.user.toString())) {
+
+        }
+        if (receivedMessage.content.startsWith())
+            receivedMessage.channel.send("Message received: " + receivedMessage.content);
     }
-    Client.login(token);
+
 });
+
+/**
+ * Login to discord
+ */
+Client.login(FileSystem.readFileSync('BotSecretToken', 'utf8'));
+
+/**
+ * This function is responsible for API calls between the frontend and the backend.
+ * @param endpoint The backend PHP file to be reached
+ * @param api The API which this call associates with
+ * @param action The action to be executed
+ * @param parameters The parameters for the action
+ * @param callback The callback for the API call, contains success, result and error
+ * @param APIs The API parameters for the API call (for API layering)
+ */
+function api(endpoint = null, api = null, action = null, parameters = null, callback = null, APIs = {}) {
+    let form = new FormData();
+    form.append("api", JSON.stringify(hook(api, action, parameters, APIs)));
+    if (window.navigator.onLine) {
+        fetch(endpoint, {
+            method: "post",
+            body: form
+        }).then(response => {
+            response.text().then((result) => {
+                if (callback !== null && api !== null && action !== null) {
+                    try {
+                        let json = JSON.parse(result);
+                        try {
+                            if (api in json) {
+                                if ("success" in json[api] && "result" in json[api]) {
+                                    callback(json[api]["success"] === true, json[api]["result"]);
+                                } else {
+                                    callback(false, "API parameters not found");
+                                }
+                            } else {
+                                callback(false, "API not found");
+                            }
+                        } catch (e) {
+                        }
+                    } catch (e) {
+                        try {
+                            callback(false, "API result isn't JSON");
+                        } catch (e) {
+                        }
+                    }
+                }
+            });
+        });
+    } else {
+        callback(false, "Offline");
+    }
+}
+
+/**
+ * This function compiles the API call hook.
+ * @param api The API to associate
+ * @param action The action to be executed
+ * @param parameters The parameters for the action
+ * @param APIs The API parameters for the API call (for API layering)
+ * @returns {FormData} API call hook
+ */
+function hook(api = null, action = null, parameters = null, APIs = {}) {
+    if (!(api in APIs)) {
+        if (api !== null && action !== null && parameters !== null) {
+            APIs[api] = {
+                action: action,
+                parameters: parameters
+            };
+        }
+    }
+    return APIs;
+}
+
+
+function webappify() {
+
+    page("home");
+}
+
+function finish() {
+    api(WEBAPPIFY_ENDPOINT, WEBAPPIFY_API, "create", {
+        flavor: get("flavor").value,
+        configuration: {
+            name: get("name-text").value,
+            description: get("description-text").value,
+            color: get("color-text").value,
+            layout: get("layout-text").value,
+            style: get("style-text").value,
+            code: {
+                app: get("code-app-text").value,
+                load: get("code-load-text").value
+            }
+        }
+    }, (success, result, error) => {
+        if (success) {
+            get("open").onclick = () => window.location = "../apps/" + result.id;
+            get("sources").onclick = () => save("WebAppBundle.zip", result.sources, "application/zip", "base64");
+            get("docker").onclick = () => save("WebAppDocker.zip", result.docker, "application/zip", "base64");
+            page("finish");
+        } else {
+            popup("An error occurred: " + error, 0, "#AA0000AA");
+        }
+    });
+}
